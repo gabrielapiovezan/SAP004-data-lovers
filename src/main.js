338209ea538631762bb60b13a2e07ecdd0776b18
Thais Durynek <thais.durynek@gmail.com>
2020-04-26 20:00:59 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(example, data.pokemon[0]);
+
+const bulbasaur = document.getElementById("bulbasaur");
+bulbasaur.innerHTML = `<img src=${data.pokemon[0].img}>`;
