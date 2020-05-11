4037b4b0ad8ca3a246d7b75b5a9d1db14ec11723
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-05-11 17:09:57 -0300

    Iniciada a função calculadora

--- src/main.js
+++ src/main.js
@@ -1,37 +1,41 @@
 /*Bloco de configurações*/
-import { searchFunc, orderList, typeFunctionConcat, checkType, height, concatFilters} from './data.js';
+import { searchFunc, orderList, typeFunctionConcat, checkType, height, concatFilters } from './data.js';
 import data from './data/pokemon/pokemon.js';
 //import { RuleTester } from 'eslint';
 const root = document.getElementById("root") // import div
 const pokemons = data.pokemon // pokemons = array
 const filtersMenu = document.getElementById("filters")
+const select = document.getElementById("select-pokemons")
 filtersMenu.style.display = "none"
+const teste = (pokemons) => {
+        const card = document.createElement("div") // cria uma nova div
+        const img = document.createElement("img") //criar elemento img
+        const cardInformation = document.createElement("div") //cria div das informações
+        const power = document.createElement("div")
+        img.src = pokemons.img // coloca o endereço da imagem
 
-/*Bloco de impressão dos pokemons*/
+        card.classList.add("card")
+        img.classList.add("picture") // coloca uma classe na imagem
+        let namePokemons = pokemons.name.replace("(Female)", "")
+        namePokemons = namePokemons.replace("(Male)", "")
+        cardInformation.innerHTML += `<br><h5>${pokemons.num}</h5><br><h3>${namePokemons}</h4>` // coloca o nameInput
+        pokemons.type.map(a => {
+            const powerType = document.createElement("div")
+            powerType.classList = (a)
+            powerType.classList.add("power")
+            powerType.innerHTML += `<h5>${a}</h5>` //${pokemons.type[1] = pokemons.type[1] || ""}</h5>` // coloca o tipo
+            power.appendChild(powerType)
+        });
+
+        cardInformation.appendChild(power)
+        card.appendChild(cardInformation, img) // coloca informações no card
+        card.appendChild(img) //coloca a imagem na nova div
+        return card
+    }
+    /*Bloco de impressão dos pokemons*/
 const print = pokemons => { //função para imprimir os pokemons
-    document.getElementById("main").style.display="block"
-    const card = document.createElement("div") // cria uma nova div
-    const img = document.createElement("img") //criar elemento img
-    const cardInformation = document.createElement("div") //cria div das informações
-    const power = document.createElement("div")
-    img.src = pokemons.img // coloca o endereço da imagem
-
-    card.classList.add("card")
-    img.classList.add("picture") // coloca uma classe na imagem
-    let namePokemons = pokemons.name.replace("(Female)", "")
-    namePokemons = namePokemons.replace("(Male)", "")
-    cardInformation.innerHTML += `<br><h5>${pokemons.num}</h5><br><h3>${namePokemons}</h4>` // coloca o nameInput
-    pokemons.type.map(a => {
-        const powerType = document.createElement("div")
-        powerType.classList = (a)
-        powerType.classList.add("power")
-        powerType.innerHTML += `<h5>${a}</h5>` //${pokemons.type[1] = pokemons.type[1] || ""}</h5>` // coloca o tipo
-        power.appendChild(powerType)
-    });
-
-    cardInformation.appendChild(power)
-    card.appendChild(cardInformation, img) // coloca informações no card
-    card.appendChild(img) //coloca a imagem na nova div
+    document.getElementById("main").style.display = "block"
+    const card = teste(pokemons)
     root.appendChild(card) // coloca nova div dentro da div existente
 }
 
@@ -55,7 +59,7 @@ const creatButtonView = () => {
 /*Tela inicial*/
 const main = () => {
     clearDisplay()
-    const elementMain = pokemons.filter((pokemons) => Number(pokemons.num) < 8)
+    const elementMain = pokemons.filter((pokemons) => pokemons.id < 8)
     elementMain.map(print)
     const viewAll = creatButtonView()
     viewAll.innerHTML = "<h3>Ver Tudo</h3>"
@@ -99,8 +103,8 @@ const typeFunction = (p) => {
 /*Função que reseta o menu avançado*/
 const resetSearch = () => {
     document.querySelectorAll("input[type=checkbox]").forEach(check => check.checked = false)
-advancedSearch()
-main()
+    advancedSearch()
+    main()
 }
 
 //função que pega os doms
@@ -117,67 +121,86 @@ const getHeight = () => {
 /*Função que filtra os checkboxs*/
 const advancedSearch = () => {
     clearDisplay()
-    const pokemonsType = pokemons.filter(typeFunction)//.map(print)
-    const heightArray = getHeight()//.map(print)
+    const pokemonsType = pokemons.filter(typeFunction) //.map(print)
+    const heightArray = getHeight() //.map(print)
     const orderBy = document.getElementById("browsers").value
-orderList(orderBy,searchName(concatFilters(pokemonsType, heightArray,pokemons))).map(print)
+    orderList(orderBy, searchName(concatFilters(pokemonsType, heightArray, pokemons))).map(print)
 
 }
-const functionMenu = ()=>{
-    document.querySelectorAll(".screen").forEach((screen)=> screen.style.display="none")
-    
-    document.getElementById("home-menu").addEventListener("click",function(){   
-        document.getElementById("home").style.display ="block"
+const functionMenu = () => {
+    document.querySelectorAll(".screen").forEach((screen) => screen.style.display = "none")
+
+    document.getElementById("home-menu").addEventListener("click", function() {
+        document.getElementById("home").style.display = "block"
     })
-    document.getElementById("comparation-menu").addEventListener("click",function(){   
-        document.getElementById("comparation").style.display ="block"
+    document.getElementById("comparation-menu").addEventListener("click", function() {
+        document.getElementById("comparation").style.display = "block"
     })
-    document.getElementById("ranking-menu").addEventListener("click",function(){   
-        document.getElementById("ranking").style.display ="block"
+    document.getElementById("ranking-menu").addEventListener("click", function() {
+        document.getElementById("ranking").style.display = "block"
     })
-    document.getElementById("main-menu").addEventListener("click",function(){   
-        document.getElementById("main").style.display ="block"
+    document.getElementById("main-menu").addEventListener("click", function() {
+        document.getElementById("main").style.display = "block"
+    })
+}
+const selectPokemons = () => {
+    pokemons.forEach((a) => {
+        let opt = document.createElement('option')
+        opt.value = a.name
+        opt.text = a.name
+        select.appendChild(opt)
     })
 }
 
+const calculator = () => {
+    pokemons.forEach((a) => {
+        if (a.name === select.value)
+            alert(select.value)
+    })
+}
 
-document.querySelectorAll(".menu-buttons").forEach(buttons => buttons.addEventListener("click",functionMenu))
+document.querySelectorAll(".menu-buttons").forEach(buttons => buttons.addEventListener("click", functionMenu))
 document.querySelectorAll('form').forEach(form => form.addEventListener('input', advancedSearch))
 document.getElementById("menu-filter").addEventListener('click', filters)
 document.getElementById("reset-search").addEventListener('click', resetSearch)
+document.getElementById("select-pokemons").addEventListener('input', calculator)
 functionMenu()
 main()
+selectPokemons()
+    //calculator()
 
 /*Ranking*/
 // Declaração de variáveis
 const typeChartHtml = document.getElementById('type-chart').getContext('2d');
 //typeChartHtml.innerHTML = ;
-const pokemonTypes = ['Flying', 'Ice', 'Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison', 
-'Electric', 'Ground', 'Rock', 'Fighting', 'Psychic', 'Ghost', 'Dragon', 'Fairy'];
+const pokemonTypes = ['Flying', 'Ice', 'Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison',
+    'Electric', 'Ground', 'Rock', 'Fighting', 'Psychic', 'Ghost', 'Dragon', 'Fairy'
+];
 const pokemonTypesColor = ['teal', 'rgb(142, 197, 233)', 'rgb(106, 230, 172)',
-'orange', 'rgb(29, 230, 209)', 'rgb(139, 238, 119)', 'rgb(250, 110, 100)', 'rgb(210, 163, 214)', 
-'rgb(241, 241, 98)', 'rgb(219, 122, 58)', 'rgb(56, 56, 56)', 'coral', 'crimson', 'darkmagenta', 
-'rgb(226, 68, 160)', 'rgb(199, 126, 218)']; 
+    'orange', 'rgb(29, 230, 209)', 'rgb(139, 238, 119)', 'rgb(250, 110, 100)', 'rgb(210, 163, 214)',
+    'rgb(241, 241, 98)', 'rgb(219, 122, 58)', 'rgb(56, 56, 56)', 'coral', 'crimson', 'darkmagenta',
+    'rgb(226, 68, 160)', 'rgb(199, 126, 218)'
+];
 //Lógica para o cálculo de porcentagem de cada tipo 
 let typeLength = [];
-for(let types of pokemonTypes){
-  let typesArray = [];
-  pokemons.filter(function(pokemon) {
-      if (pokemon.type.includes(types)){
-        typesArray.push(types)
-      }
-    return typesArray;
-    //typesArray = ['fire', 'fire', 'fire']
-});
-  typeLength.push(typesArray.length);
-  //typeLength = [5, 0, 0, 3, 4, ...]
+for (let types of pokemonTypes) {
+    let typesArray = [];
+    pokemons.filter(function(pokemon) {
+        if (pokemon.type.includes(types)) {
+            typesArray.push(types)
+        }
+        return typesArray;
+        //typesArray = ['fire', 'fire', 'fire']
+    });
+    typeLength.push(typesArray.length);
+    //typeLength = [5, 0, 0, 3, 4, ...]
 }
 
 let percent = [];
-const typesPercent = typeLength.map(function(lengthArray){
-  percent = ((lengthArray/pokemons.length)*100);
-  return percent;
-  //percent = [12.58, 3.33, 9.27, ...]
+const typesPercent = typeLength.map(function(lengthArray) {
+    percent = ((lengthArray / pokemons.length) * 100);
+    return percent;
+    //percent = [12.58, 3.33, 9.27, ...]
 });
 /*Criei um for para percorrer o array pokemonTypes. Para cada tipo dentro desse array eu faço uma função filter, que
 vai percorrer o array pokemons. Se o array pokemon incluir o tipo, insere esse tipo dentro de typesArray.
@@ -206,4 +229,4 @@ const typeChart = new Chart(typeChartHtml, {
             display: false
         }
     }
-}); 
+});
