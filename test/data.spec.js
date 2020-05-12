859e2e28d7c1ed5400a194358f610b7f1a90c6d5
Thais Durynek <thais.durynek@gmail.com>
2020-05-12 12:18:20 -0300

    mudanças nos arquivos para os testes passarem e testes para verificar se é função

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,53 @@
-import { searchFunc } from '../src/data.js';
+import { searchFunc, orderList, typeFunctionConcat, checkType, height, concatFilters} from '../src/data.js';
 
 
-/*describe('searchFunc', () => {
+describe('searchFunc', () => {
   it('is a function', () => {
     expect(typeof searchFunc).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `pikachu`', () => {
+    expect(searchFunc('pikac')).toBe('Pikachu');
+  });
+}); 
+
+describe('orderList', () => {
+  it('is a function', () => {
+    expect(typeof orderList).toBe('function');
+  });
+});
+
+describe('typeFunctionConcat', () => {
+  it('is a function', () => {
+    expect(typeof typeFunctionConcat).toBe('function');
+  });
+});
+
+describe('checkType', () => {
+  it('is a function', () => {
+    expect(typeof checkType).toBe('function');
+  });
+});
+
+describe('height', () => {
+  it('is a function', () => {
+    expect(typeof height).toBe('function');
+  });
+});
+
+describe('concatFilters', () => {
+  it('is a function', () => {
+    expect(typeof concatFilters).toBe('function');
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-}); */
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// }); 
