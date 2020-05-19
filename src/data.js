68bc4054ecbf542e5b10141de0a7eb1d1e604eaa
Thais Durynek <thais.durynek@gmail.com>
2020-05-19 11:23:26 -0300

    código indentado

--- src/data.js
+++ src/data.js
@@ -1,29 +1,16 @@
-/**Função que calcula o CP resltante
- * tem como entrada o CP do pokemon anterior e o pokemon
- */
 export const startCalculador = (cp, pokemon) => {
-    if (pokemon.multipliers) {
-        const cpResult = pokemon.multipliers.map((m) => {
-            return m * cp
-        })
+        if (pokemon.multipliers) {
+            const cpResult = pokemon.multipliers.map((m) => {
+                return m * cp
+            })
 
-        return cpResult
-    } else {
-        throw new TypeError('Digite um número');
+            return cpResult
+        } else {
+            throw new TypeError('Digite um número');
+        }
     }
-}
 
 
-/*função que ordena em ordem alfabética e num
-Tem como parâmetro o 
-Indice:
-0 - A - Z
-1 = Z - A
-2 = <
-3 = >
-Array: a ser organizado
-retorna o tipo de array solicitado ordenado
-*/
 export const orderList = (orderBy, nameArray) => {
         let newArray = [];
         let objectParemeter
@@ -37,72 +24,55 @@ export const orderList = (orderBy, nameArray) => {
             return newArray
         }
     }
-    /*Função que ordena elementos em ordem crescente 
-    Tem como parâmetro:
-    -Array a ser organizado
-    -Parametro do objeto(name,num,id...)
-    retorna um array ordenado*/
+
+    
 export const order = (nameArray, objectParemeter) => {
-    return nameArray.sort(function(a, b) {
-        return ((a[objectParemeter] > b[objectParemeter]) ? 1 : ((b[objectParemeter] > a[objectParemeter]) ? -1 : 0))
-    })
-}
-
-/*Concatena os filtros:
-tem como parâmetros:
-os três arrays, dois resultados de filtros a serem concatenados e um array princial
-retorna um array com os os elementos dos dois filtros iguais
-caso nenhum item tenha sidoselecionado nos filtros, retorna o array principal*/
-export const concatFilters = (pokemonsType, heightArray, pokemons) => {
-    let resultFilters = []
-    if (pokemonsType.length && heightArray.length) {
-        resultFilters = pokemonsType.filter((pokemonsType) => {
-            for (let i of heightArray) {
-                if (pokemonsType === i)
-                    return true
-            }
+        return nameArray.sort(function(a, b) {
+            return ((a[objectParemeter] > b[objectParemeter]) ? 1 : ((b[objectParemeter] > a[objectParemeter]) ? -1 : 0))
         })
-    } else
-        resultFilters = pokemonsType.concat(heightArray)
-    if (!resultFilters.length)
-        resultFilters = pokemons
-    return resultFilters
-}
+    }
 
-/*Função que divide em intervalos as alturas dos pokemons 
-tirando os ultimos elementos que apresentavam um desvio padrão muito alto
-recebe como parâmetro:
- o valor da altura desejado, recebido através de um checkbox
-l- pequeno
-m - médio
-b - grande 
-o array a ser calculado
-retorna um array com os intervalos selecionados*/
+
+export const concatFilters = (pokemonsType, heightArray, pokemons) => {
+        let resultFilters = []
+        if (pokemonsType.length && heightArray.length) {
+            resultFilters = pokemonsType.filter((pokemonsType) => {
+                for (let i of heightArray) {
+                    if (pokemonsType === i)
+                        return true
+                }
+            })
+        } else
+            resultFilters = pokemonsType.concat(heightArray)
+        if (!resultFilters.length)
+            resultFilters = pokemons
+        return resultFilters
+    }
+
+
 export const resultPokemons = (pokemons, resultMax, resultMin) => pokemons.filter((p) => {
-    if (resultMin < parseFloat(p.height) && parseFloat(p.height) < resultMax) // array 4 pois os 5 primeiros estão muito acima da média
-        return true
-})
+        if (resultMin < parseFloat(p.height) && parseFloat(p.height) < resultMax) // array 4 pois os 5 primeiros estão muito acima da média
+            return true
+        })
 
-/*pesquisa uma palavra, recebe como parâmetro um array e uma string
-ele pesquisa a string no parâmetro*/
+
 export const searchFunc = (p, nameInput, parameter) => {
-    nameInput = nameInput.toUpperCase()
-    return p.filter((p) => {
-        return nameInput === p[parameter].slice(0, -(p[parameter].length - nameInput.length)).toUpperCase() || nameInput === p[parameter].toUpperCase()
-    })
-
-}
+        nameInput = nameInput.toUpperCase()
+        return p.filter((p) => {
+            return nameInput === p[parameter].slice(0, -(p[parameter].length - nameInput.length)).toUpperCase() || nameInput === p[parameter].toUpperCase()
+        })
+    }
 
 export const percentage = (pokemonKeysArray, key, pokemonArray) => {
-    let percentageArray = [];
-    for(let item of pokemonKeysArray){
-      let includesArray = [];
-      pokemonArray.filter(function(pokemon){
-        if(pokemon[key].includes(item)){
-          return includesArray.push(item);
-        }
-      });
-      percentageArray.push((includesArray.length / pokemonArray.length)*100); 
-    } 
-    return percentageArray;
-  };
+        let percentageArray = []
+        for(let item of pokemonKeysArray){
+        let includesArray = []
+        pokemonArray.filter(function(pokemon){
+            if(pokemon[key].includes(item)){
+            return includesArray.push(item)
+            }
+        });
+        percentageArray.push((includesArray.length / pokemonArray.length)*100) 
+        } 
+        return percentageArray
+    }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { searchFunc, orderList, resultPokemons, concatFilters, startCalculador, order, percentage } from './data.js';
 import data from './data/pokemon/pokemon.js';
-const root = document.getElementById("root") // import div
-const pokemons = data.pokemon // pokemons = array
+const root = document.getElementById("root") 
+const pokemons = data.pokemon 
 const filtersMenu = document.getElementById("filters")
 const select = document.getElementById("select-pokemons")
 const calculatorMain = document.getElementById("calculator-main")
@@ -44,12 +44,12 @@ const creatCard = (pokemons, info = [], infoEx) => {
         const powerType = document.createElement("div")
         powerType.classList = (a)
         powerType.classList.add("power")
-        powerType.innerHTML += `<h5>${a}</h5>` //${pokemons.type[1] = pokemons.type[1] || ""}</h5>` // coloca o tipo
+        powerType.innerHTML += `<h5>${a}</h5>`
         power.appendChild(powerType)
     });
-    card.appendChild(img) //coloca a imagem na nova div
+    card.appendChild(img) 
     cardInformation.appendChild(power)
-    card.appendChild(cardInformation) // coloca informações no card
+    card.appendChild(cardInformation)
     return card
 }
 
@@ -320,6 +320,22 @@ const calculator = (pokemons) => {
     creatCardCalculator(pokemon[0], evolutuin, cp, cpResult)
 }
 
+const hourFunction = () => {
+    let time = new Date().getHours();
+    const filterSpawnTime = pokemons.filter(pokemon => {
+        if(time === Number(pokemon.spawn_time.slice(0,-3))){
+            return true;
+        }
+    });
+    filterSpawnTime.forEach(pokemon => {
+        const card = creatCard(pokemon);
+        card.addEventListener("click", () => {
+            createModal(pokemon)
+        });
+        document.getElementById('hour').appendChild(card);
+    })
+}
+
 
 document.querySelectorAll(".menu-buttons").forEach(buttons => buttons.addEventListener("click", functionMenu))
 document.querySelectorAll('form').forEach(form => form.addEventListener('input', advancedSearch))
@@ -336,6 +352,7 @@ document.getElementById("menu-open").addEventListener('click', () => {
     menu.style.display === "none" ? menu.style.display = "block" : menu.style.display = "none"
     main()
 })
+hourFunction();
 
 functionMenu()
 main()
@@ -343,7 +360,7 @@ selectPokemons()
 
 
 /*Ranking*/
-//Declaração de variáveis
+
 const pokemonTypes = ['Flying', 'Ice', 'Grass', 'Fire', 'Water', 'Bug', 'Normal', 'Poison',
     'Electric', 'Ground', 'Rock', 'Fighting', 'Psychic', 'Ghost', 'Dragon', 'Fairy'
 ];
@@ -354,13 +371,9 @@ const pokemonTypesColor = ['teal', 'rgb(142, 197, 233)', 'rgb(106, 230, 172)',
 ];
 
 const typePercentage = percentage(pokemonTypes, "type", pokemons);
-//const weaknessPercentage = percentage(pokemonTypes, "weaknesses", pokemons);
 
-// Função Gráfico
 new Chart(document.getElementById('type-chart').getContext('2d'), {
-    //o tipo de gráfico
     type: 'bar',
-    //dados do gráfico
     data: {
         labels: pokemonTypes,
         datasets: [{
@@ -368,7 +381,6 @@ new Chart(document.getElementById('type-chart').getContext('2d'), {
             data: typePercentage
         }]
     },
-    // Configuração de título - display: true faz o título aparecer na tela
     options: {
         title: {
             text: 'Porcentagem de tipos de Pokémons',
